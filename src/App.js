3fdba3c5966bc27c1a04dd1760c2214b30389a90
guntz <lambo1234@hotmail.com>
2023-03-06 22:50:28 +0700

    Workshop2

--- src/App.js
+++ src/App.js
@@ -1,22 +1,9 @@
-import {createContext, useState} from "react";
-import Menu from "./components/QuizApp/Menu"
-import Quiz from "./components/QuizApp/Quiz"
-import Score from "./components/QuizApp/Score"
 import './App.css'
-
-export const DataContext = createContext();
-
 function App() {
-  const [appState,setAppState] = useState("menu")
-  return (
-    <DataContext.Provider value={{appState,setAppState}}>
-      <div className="App">
-          {appState === "menu" && <Menu/>}
-          {appState === "quiz" && <Quiz/>}
-          {appState === "score" && <Score/>}
-      </div>
-    </DataContext.Provider>
 
+  return (
+      <div className="App">
+      </div>
   );
 }
 
---
+++ src/App_Quiz.js
@@ -0,0 +1,25 @@
+import {createContext, useState} from "react";
+import Menu from "./components/QuizApp/Menu"
+import Quiz from "./components/QuizApp/Quiz"
+import Score from "./components/QuizApp/Score"
+import './App_Quiz.css'
+
+export const DataContext = createContext();
+
+function App() {
+  const [appState,setAppState] = useState("menu")
+  const [score,setScore] = useState(0);
+  return (
+    <DataContext.Provider value={{appState,setAppState,score,setScore}}>
+      <div className="App">
+        <h1>Quiz Application </h1>
+          {appState === "menu" && <Menu/>}
+          {appState === "quiz" && <Quiz/>}
+          {appState === "score" && <Score/>}
+      </div>
+    </DataContext.Provider>
+
+  );
+}
+
+export default App;
