fe4bb1a05c4ef1824ba758a83b0d11856aabc1d0
lambo1112 <44872757+lambo1112@users.noreply.github.com>
2023-03-06 15:05:24 +0700

    workshop2.1

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
-import FormComponent from "./components/Formcomponent";
+import {createContext, useState} from "react";
+import Menu from "./components/QuizApp/Menu"
+import Quiz from "./components/QuizApp/Quiz"
+import Score from "./components/QuizApp/Score"
+import './App.css'
+
+export const DataContext = createContext();
 
 function App() {
+  const [appState,setAppState] = useState("menu")
   return (
-    <div className="App">
-      <FormComponent/>
-    </div>
+    <DataContext.Provider value={{appState,setAppState}}>
+      <div className="App">
+          {appState === "menu" && <Menu/>}
+          {appState === "quiz" && <Quiz/>}
+          {appState === "score" && <Score/>}
+      </div>
+    </DataContext.Provider>
+
   );
 }
 
